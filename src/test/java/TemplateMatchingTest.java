ac31ff975a5fa092d401ca7a2ef12a20e1b84228
Christian Tischer <christian.tischer@embl.de>
2018-08-26 17:54:28 +0200

    Add working template matching test

--- src/test/java/TemplateMatchingTest.java
+++ src/test/java/TemplateMatchingTest.java
@@ -3,7 +3,9 @@ import ij.*;
 import ij.measure.Calibration;
 import ij.process.*;
 import ij.gui.*;
-import java.awt.Color;
+
+import java.awt.*;
+
 import ij.plugin.*;
 import ij.plugin.filter.MaximumFinder;
 import ij.measure.ResultsTable;
@@ -28,23 +30,36 @@ public class TemplateMatchingTest
 
 		final double overviewPixelWidth = overview.getCalibration().pixelWidth;
 
-		final Scaler scaler = new Scaler();
+		ArrayList< int[] > offsets = new ArrayList<>(  );
 
 		for ( ImagePlus template : templates )
 		{
 			final double templatePixelWidth = template.getCalibration().pixelWidth;
 			final double scale = templatePixelWidth / overviewPixelWidth;
 
-			IJ.run(template, "Scale...", "x="+scale+" y="+scale+" interpolation=Bilinear average");
+			IJ.run( template, "Scale...", "x="+scale+" y="+scale+" interpolation=Bilinear average");
+
+			IJ.run( template, "Rotate... ", "angle=11.50 grid=1 interpolation=Bilinear");
+			template.setRoi(915,916,218,214);
+			IJ.run( template, "Crop", "");
 
 			template.show();
+
+			FloatProcessor rFp = doMatch( overview, template, 5, true );
+
+			offsets.add( findMax(rFp, 0) );
+
 		}
 
-		long startTime = System.currentTimeMillis();
+		overview.show();
 
-//		FloatProcessor rFp = doMatch( img1, img2, method, showR );
+		Overlay overlay = new Overlay(  );
+		for ( int[] offset : offsets )
+		{
+			overlay.add( new Roi( offset[0], offset[1], templates.get( 0 ).getWidth(), templates.get( 0 ).getHeight() ));
+		}
 
-		long elapsedTime = System.currentTimeMillis() - startTime;
+		overview.setOverlay( overlay );
 
 
 	}
